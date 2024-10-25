1ed4aef8c861bc1a537f8d019f0d7c3574611485
Alena Klekovkina <50324020+AlyonaKlekovkina@users.noreply.github.com>
2024-10-25 15:33:31 +0200

    Update main.py
    Stage 3/5

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Write your code here
 import random
 
 
@@ -26,7 +25,7 @@ def create_starting_set():
     return players_set, computers_set, stock
 
 
-#checks if there are doubles and returns highest doubles
+#checks if there are doubles and returns highest double (only one of the pair and the index where it is located in set
 def find_all_doubles_in_set(hand_to_find_doubles):
     index = 0
     highest = 0
@@ -37,25 +36,97 @@ def find_all_doubles_in_set(hand_to_find_doubles):
     return highest, index
 
 
+#forms a print statement
+def print_result(stock, computers_set, players_set, snake):
+    print('='*70)
+    print('Stock size:', len(stock))
+    print('Computer pieces:', len(computers_set))
+    print()
+    if len(snake) <= 6:
+        print(*snake)
+    else:
+        print(snake[0], snake[1], snake[2], "...", snake[-3], snake[-2], snake[-1])
+    print()
+    print('Your pieces:')
+    for i in range(len(players_set)):
+        print('{}:{}'.format(i+1, players_set[i]))
+    print()
+
+
+def determine_first_move(players_set, players_doubles, computers_set, computers_doubles, snake):
+    if players_doubles[0] > computers_doubles[0]:
+        the_index = players_doubles[1]
+        the_piece = players_set[the_index]
+        players_set.pop(the_index)
+        snake.append(the_piece)
+        return 'player', snake, the_index
+    if computers_doubles[0] > players_doubles[0]:
+        the_index = computers_doubles[1]
+        the_piece = computers_set[the_index]
+        snake.append(the_piece)
+        computers_set.pop(the_index)
+        return 'computer', snake, the_index
+
+
+def make_move(the_set, stock, snake, move):
+    if len(move) == 0:
+        snake.append(the_set[0])
+        the_set.pop(0)
+    else:
+        try:
+            int(move)
+        except ValueError:
+            print("Invalid input. Please try again.")
+            move = input()
+        if abs(int(move)) > len(the_set):
+            move = input()
+        if len(move) == 0:
+            snake.append(the_set[0])
+            the_set.pop(0)
+        if int(move) == 0:
+            the_set.append(stock[0])
+            stock.pop(0)
+            return the_set
+        if int(move) > 0:
+            snake.append(the_set[int(move) - 1])
+        elif int(move) < 0:
+            snake.insert(0, the_set[abs(int(move)) - 1])
+        the_set.pop(abs(int(move)) - 1)
+
+
 starting_tool = create_starting_set()
 players_set = starting_tool[0]
 computers_set = starting_tool[1]
 stock = starting_tool[2]
 players_doubles = find_all_doubles_in_set(players_set)
 computers_doubles = find_all_doubles_in_set(computers_set)
+snake = []
+first_move = determine_first_move(players_set, players_doubles, computers_set, computers_doubles, snake)
+starting_player = first_move[0]
+starting_index = first_move[2]
 
-
-if players_doubles[0] > computers_doubles[0]:
-    print('Stock pieces: ', stock)
-    print('Computer pieces: ', computers_set)
-    players_set.pop(players_doubles[1])
-    print('Player pieces: ', players_set)
-    print('Domino snake: [[{}, {}]]'.format(players_doubles[0], players_doubles[0]))
-    print('Status: computer')
-if computers_doubles[0] > players_doubles[0]:
-    print('Stock pieces: ', stock)
-    computers_set.pop(computers_doubles[1])
-    print('Computer pieces: ', computers_set)
-    print('Player pieces: ', players_set)
-    print('Domino snake: [[{}, {}]]'.format(computers_doubles[0], computers_doubles[0]))
-    print('Status: player')
+if starting_player == 'computer':
+    count = 1
+elif starting_player == 'player':
+    count = 0
+while len(players_set) != 0 or len(computers_set) != 0:
+    if count % 2 == 0:
+        print_result(stock, computers_set, players_set, snake)
+        print("Status: Computer is about to make a move. Press Enter to continue...")
+        move = input()
+        make_move(computers_set, stock, snake, move)
+        count += 1
+        if len(computers_set) == 0:
+            print_result(stock, computers_set, players_set, snake)
+            print("Status: The game is over. The computer won!")
+            break
+    if count % 2 != 0:
+        print_result(stock, computers_set, players_set, snake)
+        print("Status: It's your turn to make a move. Enter your command.")
+        move = input()
+        make_move(players_set, stock, snake, move)
+        count += 1
+        if len(players_set) == 0:
+            print_result(stock, computers_set, players_set, snake)
+            print("Status: The game is over. You won!")
+            break
